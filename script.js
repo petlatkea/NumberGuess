5fc7f1079673ac66d1194600e80557a15dbe4dd7
Peter Lind <petl@kea.dk>
2024-08-26 15:27:50 +0200

    Finished game - with random

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
 window.addEventListener("load", start);
 
+let secret;
+
 function start() {
   console.log("JavaScript kører!");
+  secret = randomNumber();
+  document.querySelector("#guessform").addEventListener("submit", receiveInput);
+}
+
+function randomNumber() {
+  return Math.floor(Math.random()*100);
+  return 42;
+}
+
+function receiveInput(event) {
+  event.preventDefault();
+  const form = event.target;
+  const guess = form.guess.valueAsNumber;
+
+  const comp = compare(guess,secret);
+  if(comp < 0) {
+    outputAnswer(`Du gættede på ${guess} - det var for lavt`);
+  }  
+  if(comp > 0) {
+    outputAnswer(`Du gættede på ${guess} - det var for højt`);
+  }
+
+  if( comp == 0) {
+    outputAnswer(`Du gættede ${guess} og det er korrekt`);
+    document.querySelector("#guessform").removeEventListener("submit", receiveInput);
+    document.querySelector("#guessform").remove();
+  }
+}
+
+function compare(guess, secret) {
+  return guess-secret;
+}
+
+function outputAnswer(text) {
+  document.querySelector("#guesses").insertAdjacentHTML("beforeend", `<li>${text}</li>`);
+}
+
+function CSSConfetti() {
+  
 }
